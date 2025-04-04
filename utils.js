cfabd4e34b1d419738b73303d9e56d19c0e4a895
Askhat Bikmetov <askhat@hey.com>
2025-04-04 20:45:50 +0300

    remove any traces of sass

--- utils.js
+++ utils.js
@@ -1,4 +1,3 @@
-const sass = require('node-sass')
 const path = require('path')
 const fs = require('fs')
 
@@ -8,58 +7,6 @@ const ensureDirectoryExistence = (filePath) => {
     return true
   }
   fs.mkdirSync(dirname, { recursive: true })
-}
-
-/**
- * Converts sass types to plain strings that can be read again by the sass compiler
- * @param value
- * @returns {string}
- */
-const sassToString = (value) => {
-  let val = ''
-  if (value instanceof sass.types.Color) {
-    const a = parseInt(value.getA() * 255)
-    if (a === 0) {
-      val = 'transparent'
-    } else {
-      val =
-        '#' +
-        parseInt(value.getR()).toString(16).padStart(2, 0) +
-        parseInt(value.getG()).toString(16).padStart(2, 0) +
-        parseInt(value.getB()).toString(16).padStart(2, 0) +
-        (a === 255 ? '' : a.toString(16).padStart(2, 0))
-
-      let short = true
-      let shortVal = '#'
-      for (let i = 1; i < (a === 255 ? 6 : 8); i += 2) {
-        if (val[i] !== val[i + 1]) {
-          short = false
-        } else {
-          shortVal += val[i]
-        }
-      }
-      if (short) val = shortVal
-    }
-  } else if (value instanceof sass.types.List) {
-    for (let i = 0; i < value.getLength(); ++i) {
-      val += sassToString(value.getValue(i)) + ' '
-    }
-    val = '(' + val.slice(0, -1) + ')'
-  } else if (value instanceof sass.types.Map) {
-    for (let i = 0; i < value.getLength(); ++i) {
-      val += value.getKey(i) + ':' + sassToString(value.getValue(i)) + ','
-    }
-    val = '(' + val.slice(0, -1) + ')'
-  } else if (value instanceof sass.types.Null) {
-    val = ''
-  } else if (value instanceof sass.types.Number) {
-    val = value.getValue().toPrecision(2) + value.getUnit()
-  } else if (value instanceof sass.types.String) {
-    val = '"' + value.getValue() + '"'
-  } else {
-    val = value.getValue()
-  }
-  return val
 }
 
 const postcss = require('postcss')
@@ -124,29 +71,6 @@ const writeOutput = async (output, result, options) => {
   await Promise.all(p)
 }
 
-const renderSassSync = (input, output, variables, functions) => {
-  variables = variables || {}
-  let data = Object.keys(variables)
-    .map((v) => '$' + v + ':' + variables[v] + ';')
-    .join('\n')
-
-  data += '\n@import "' + input + '";'
-  try {
-    return sass.renderSync({
-      data,
-      outFile: output + '.css',
-      includePaths: [path.resolve(process.cwd(), input)],
-      outputStyle: 'expanded',
-      sourceMap: true,
-      sourceMapContents: true,
-
-      functions,
-    })
-  } catch (e) {
-    console.error(e)
-  }
-}
-
 const watched = []
 const watch = (render, cb) => {
   render.stats.includedFiles.map((file) => {
@@ -176,8 +100,6 @@ const partition = (array, isValid) => {
 module.exports = {
   ensureDirectoryExistence,
   writeOutput,
-  renderSassSync,
-  sassToString,
   partition,
   watch,
   unwatch,
